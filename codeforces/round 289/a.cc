9d7e0031a3c81646f648cec7d95b404ed226b4b9
Holger Thies <info@holgerthies.com>
2015-02-04 06:26:31 +0100

    a

---
+++ codeforces/round 289/a.cc
@@ -0,0 +1,50 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <set>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <limits>
+#include <tuple>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <iterator>
+#include <string>
+#include <fstream>
+#define FOR(k,a,b) for(typeof(a) k=(a); k <= (b); ++k)
+#define REP(k,a) for(int k=0; k < (a); ++k)
+#define INF 200000000
+#define mp make_pair
+#define len(s) (int)((s).size())
+#define pb push_back
+using namespace std;
+typedef long long ll;
+typedef unsigned int uint;
+using vi = vector<int>;
+using vii = vector<vector<int>>;
+using pii = pair<int,int>;
+
+int ncr(int n, int r){
+	if(r == 0) return 1;
+	return (n*ncr(n-1, r-1))/r;
+}
+
+int main(){
+	int n;
+	cin >> n;
+	cout << ncr(2*(n-1), n-1)<<endl;
+	return 0;
+}
