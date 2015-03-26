12fd4d4b72cf5cf3e9faef6ef65908cfa845631e
Holger Thies <info@holgerthies.com>
2015-03-26 22:09:07 +0100

    a & b

---
+++ codeforces/round 297/a.cc
@@ -0,0 +1,63 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <set>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <limits>
+#include <tuple>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <iterator>
+#include <string>
+#include <fstream>
+#define REP(k,a) for(int k=0; k < (a); ++k)
+#define REPP(k,a,b) for(int k= (a); k < (b); ++k)
+#define INF 200000000
+#define mp make_pair
+#define len(s) (int)((s).size())
+#define pb push_back
+#define all(X) (X).begin(), (X).end()
+#define DRI(X) int (X); scanf("%d", &X)
+#define DRII(X, Y) int X, Y; scanf("%d%d", &X, &Y)
+#define DRIII(X, Y, Z) int X, Y, Z; scanf("%d%d%d", &X, &Y, &Z)
+
+using namespace std;
+typedef long long ll;
+typedef unsigned int uint;
+using vi = vector<int>;
+using vii = vector<vector<int>>;
+using pii = pair<int,int>;
+template <class T>
+void print(vector<T> v){ bool first=true; for(T x : v) { if(!first) cout << " "; first = false; cout << x; } cout << endl;}
+int main(){
+	DRI(n);
+	vector<int> h(26, 0);
+	string m;
+	cin >> m;
+	int c=0;
+	REP(i, len(m)){
+		if(islower(m[i])){
+			h[m[i]-'a']++;
+		} else{
+			if(!h[m[i]-'A']) c++;
+			else
+				h[m[i]-'A']--;
+		}
+	}
+	cout << c << endl;
+	return 0;
+}
---
+++ codeforces/round 297/b.cc
@@ -0,0 +1,66 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <set>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <limits>
+#include <tuple>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <iterator>
+#include <string>
+#include <fstream>
+#define REP(k,a) for(int k=0; k < (a); ++k)
+#define REPP(k,a,b) for(int k= (a); k < (b); ++k)
+#define INF 200000000
+#define mp make_pair
+#define len(s) (int)((s).size())
+#define pb push_back
+#define all(X) (X).begin(), (X).end()
+#define DRI(X) int (X); scanf("%d", &X)
+#define DRII(X, Y) int X, Y; scanf("%d%d", &X, &Y)
+#define DRIII(X, Y, Z) int X, Y, Z; scanf("%d%d%d", &X, &Y, &Z)
+
+using namespace std;
+typedef long long ll;
+typedef unsigned int uint;
+using vi = vector<int>;
+using vii = vector<vector<int>>;
+using pii = pair<int,int>;
+template <class T>
+void print(vector<T> v){ bool first=true; for(T x : v) { if(!first) cout << " "; first = false; cout << x; } cout << endl;}
+
+int main(){
+	string s;
+	cin >> s;
+	int n=len(s);
+	DRI(m);
+	vector<bool> turn(n+1);
+	REP(i, m){
+		DRI(j);
+		turn[j-1] = !turn[j-1];
+		turn[n-j+1] = !turn[n-j+1];
+	}
+	bool t=false;
+	REP(i, n){
+		if(turn[i]) t = !t;
+		if(t) cout << s[n-1-i];
+		else cout << s[i];
+	}
+	cout << endl;
+	return 0;
+}
